e077adc90a49ac7d0fa8837b5c8bc0d92d7f2266
Victor Ludvig <victor.ludvig@ecl20.ec-lyon.fr>
2024-05-05 23:37:48 +0200

    update repos

--- reinforcement_learning/a2c_sb3_cartpole.py
+++ reinforcement_learning/a2c_sb3_cartpole.py
@@ -3,19 +3,21 @@ import gymnasium as gym
 from stable_baselines3 import A2C
 import wandb
 import tensorboard
+from huggingface_hub import PyTorchModelHubMixin
 
-wandb.init(project="a2c_training", sync_tensorboard=True)
+wandb.init(project='a2c_training', sync_tensorboard=True)
 
-env = gym.make("CartPole-v1", render_mode="rgb_array")
+env = gym.make('CartPole-v1', render_mode='rgb_array')
 
-model = A2C("MlpPolicy", env, verbose=1, tensorboard_log='./a2c_cartpole_tensorboard')
-model.learn(total_timesteps=25000, tb_log_name="training")
+model = A2C('MlpPolicy', env, verbose=1, tensorboard_log='./a2c_cartpole_tensorboard')
+model.learn(total_timesteps=25000, tb_log_name='training')
 
 vec_env = model.get_env()
 obs = vec_env.reset()
 for i in range(1000):
     action, _state = model.predict(obs, deterministic=True)
     obs, reward, done, info = vec_env.step(action)
-    vec_env.render("human")
+    vec_env.render('human')
 
-model.save("trained_a2c/a2c_cartpole")
+model.save('trained_a2c/a2c_cartpole')
+model.save_pretrained('my-awesome-model')
